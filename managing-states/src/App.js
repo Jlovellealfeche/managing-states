38f645ae8359e54c85450780646544fd5fdf5eca
joan lovelle alfeche <jlovellealfeche@gmail.com>
2023-03-20 08:50:13 +0100

    .

---
+++ managing-states/src/App.js
@@ -0,0 +1,15 @@
+
+import Fruits from "./Fruits";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Where should the state go?</h1>
+      <Fruits />
+
+
+    </div>
+  )
+}
+
+export default App;
